78856a6c7222a4abf68e6214f2ae7fff30c3252f
msjang <ms.jang@albamapp.com>
2017-09-22 15:12:56 +0900

    Singleton Pattern

---
+++ src/singleton/ChocolateBoiler.java
@@ -0,0 +1,53 @@
+package singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private volatile static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (ChocolateBoiler.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new ChocolateBoiler();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // 보일러에 우유/초콜릿을 혼합한 재료를 집어넣음
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // 끓인 재료를 다음 단계로 넘김
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // 재료를 끓임
+            boiled = true;
+        }
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
